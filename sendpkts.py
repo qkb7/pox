2baadade8b2f8700be7372992fe93ac46f99d764
Quinn Burke <quinnburke7@gmail.com>
2019-11-20 00:52:51 -0500

    wip : flooder more

--- sendpkts.py
+++ sendpkts.py
@@ -10,18 +10,61 @@
 
 ### Imports ###
 from random import randint
-from scapy.all import *
+# from scapy.all import *
 import time
 import threading
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 ### Classes ###
+class Flow():
+    def __init__(self, bytes_left=0, rate=0, duration=0):
+        self.bytes_left = bytes_left
+        self.rate = rate
+        self.duration = duration
 
 
 ### Functions ###
-def generateFlows(_sim_time):
-    return [0, 1]
+def generateFlows(_sim_time, _client_rate):
+    num_flows = _sim_time * _client_rate
+
+    # See https://docs.scipy.org/doc/numpy-1.15.0/reference/generated/numpy.random.pareto.html for generating samples with numpy pareto; specifically, using the formula below to obtain classical pareto from the pareto2/lomax dist
+
+    # generate sizes
+    xm1 = 10.0  # scale
+    a1 = 1.2  # shape
+    sizes = sorted((np.random.pareto(a1, num_flows) + 1) * xm1)
+
+    # generate durations
+    xm2 = 0.001
+    a2 = 1.5
+    durs = sorted((np.random.pareto(a2, num_flows) + 1) * xm2)
+
+    # sort/match to create flows
+    flows = [None]*num_flows
+    for i in range(num_flows):
+        flows[i] = Flow(sizes[i], sizes[i]/durs[i], durs[i])
+
+    cdf_sz = calc_cdf_fast(sizes)
+    cdf_durs = calc_cdf_fast(durs)
+    plt.plot(sizes, cdf_sz, color='green')
+    plt.plot(durs, cdf_durs, color='red')
+    plt.show()
+
+    return flows
+
+
+def calc_cdf_fast(arr):
+    cdf = []
+    for val in arr:
+        count = 0
+        for other_val in arr:
+            if other_val <= val:
+                count += 1
+        cdf.append(float(count*1.0/len(arr)))
+    return cdf
 
 
 def flood(_flows, flooder_idx):
@@ -42,9 +85,11 @@ def flood(_flows, flooder_idx):
 
 if __name__ == '__main__':
     # Note: only one server known to flooders "10.0.0.100"
-    sim_time = 60  # in seconds
-    flows = generateFlows(sim_time)
-    num_flooders = 10
+    sim_time = 180  # in seconds
+    client_rate = 1000  # new incoming flows per second
+    flows = generateFlows(sim_time, client_rate)
+    exit(0)
+    num_flooders = 1
     nf = len(flows)/num_flooders  # evenly divide flows for each flooder
 
     flooders = [None]*num_flooders
